d3b192cc71bca8c10c601cc07a8367a570165faa
Amanda Brown <amanda.brown@gmail.com>
2016-10-20 09:41:30 -0400

    finished

---
+++ Magic8Ball/scripts/JavaScript.js
@@ -0,0 +1,7 @@
+﻿
+function getAnswer()
+{
+    var answer = ["Ready...Set...No!", "I had fun once. I hated it.", "Try again never.", "Not bloody likely.", "Umm, no.", "I heard you, I just don't care." ];
+    var rand = answer[Math.floor(Math.random() * answer.length)];
+    document.getElementById("option").innerHTML = rand;
+}
